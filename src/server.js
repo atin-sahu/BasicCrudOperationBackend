0f48c87c64cbe283249b7c62b75fbf9290b1ce55
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-03 14:54:20 +0530

    done

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const dotenv = require('dotenv');
+const cors = require("cors");
+app.use(cors());
 require("dotenv").config();
 
 
 const connectDB = require("./config/db");
 const app = require("./index");
+
+app.get("", (req, res) => {
+  try {
+    res.send("homepage");
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.listen(5000 || process.env.PORT, async () => {
     try {
