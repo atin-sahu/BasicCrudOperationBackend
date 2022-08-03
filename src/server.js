f591083e5bfb2ff6508fa228278575bc00b34ba6
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-03 14:36:45 +0530

    done

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 
+
+// webpack.config.js
+const Dotenv = require('dotenv-webpack');
+
+// module.exports = {
+//   ...
+//   plugins: [
+//     new Dotenv()
+//   ]
+//   ...
+// };
+
 const connectDB = require("./config/db");
 const app = require("./index");
 
-app.listen(5000, async () => {
+app.listen(5000 || process.env.port, async () => {
     try {
       await connectDB();
       console.log("listening port 5000");
@@ -10,3 +22,5 @@ app.listen(5000, async () => {
       console.log(err);
     }
   });
+
+
