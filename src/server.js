a39ec729d2983ffe980238e979e717be94a6c54d
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-03 15:11:20 +0530

    done

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const connectDB = require("./config/db");
 const app = require("./index");
 
 
-app.listen(5000 || process.env.PORT, async () => {
+app.listen(process.env.PORT || 5000, async () => {
     try {
       await connectDB();
       console.log("listening port 5000");
