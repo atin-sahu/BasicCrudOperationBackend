fd9d0fa93d5f5a129571de88ac6b3fd4fe5c0d7d
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-03 15:07:59 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+app.use(cors());
 const userController = require("./controller/user.controller");
 const productController = require("./controller/product_controller");
 const app = express();
+
+app.get("", (req, res) => {
+  try {
+    res.send("homepage");
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 app.use(express.json());
 app.use("/users", userController);
--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 // const dotenv = require('dotenv');
-const cors = require("cors");
-app.use(cors());
-require("dotenv").config();
 
+
+require("dotenv").config();
 
 const connectDB = require("./config/db");
 const app = require("./index");
 
-app.get("", (req, res) => {
-  try {
-    res.send("homepage");
-  } catch (error) {
-    res.send(error);
-  }
-});
 
 app.listen(5000 || process.env.PORT, async () => {
     try {
